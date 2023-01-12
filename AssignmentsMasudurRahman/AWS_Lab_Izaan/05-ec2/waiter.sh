ce07e03e8ce7011ed98afe78d4e7aa05c88be027
Masudthedevops <rahmanmdm16@gmail.com>
2023-01-11 23:50:44 -0600

    Ec2 stack update for windows 2012

--- AssignmentsMasudurRahman/AWS_Lab_Izaan/05-ec2/waiter.sh
+++ AssignmentsMasudurRahman/AWS_Lab_Izaan/05-ec2/waiter.sh
@@ -5,3 +5,8 @@ aws cloudformation create-stack --stack-name ec2lab5 --template-body file://ec2L
 aws cloudformation wait stack-create-complete --stack-name ec2lab5
 
 aws cloudformation describe-stack-resources --stack-name ec2lab5
+
+
+aws cloudformation update-stack --stack-name ec2lab5 --template-body file://update-ec2lab5.yaml --parameters file://updateec2parameter.json
+
+aws cloudformation validate-template --template-body file://update-ec2lab5.yaml
